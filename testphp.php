cf8018f7083176c2d56933e87d095ee8477cfb12
rodmako <87238715+rodmako@users.noreply.github.com>
2022-01-27 17:34:06 +0900

    Add files via upload

---
+++ testphp.php
@@ -0,0 +1,14 @@
+<?php
+  $testtext = "oradiorada";
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>testphp</title>
+   </head>
+   <body>
+     <p><?=$testtext?></p>
+   </body>
+ </html>
